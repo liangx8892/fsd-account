8709318f88bbc1013948eb0f9182309d60c7ece2
LIANG XIONG <xionglwh@cn.ibm.com>
2019-11-07 23:28:08 +0800

    update

--- src/main/java/com/fsd/sba/utils/EncrytedPasswordUtils.java
+++ src/main/java/com/fsd/sba/utils/EncrytedPasswordUtils.java
@@ -2,17 +2,23 @@ package com.fsd.sba.utils;
 
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 public class EncrytedPasswordUtils {
 
 	public static String encrytePassword(String password) {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		PasswordEncoder encoder = new Pbkdf2PasswordEncoder();
 		return encoder.encode(password);
 	}
 
 	public static boolean matchPassword(String password, String hashpassword) {
 		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		return encoder.matches(password, hashpassword);
+	}
+	
+	public static void main(String[] args) {
+		//System.out.println(matchPassword("zaq12wsx", "{bcrypt}$2a$10$bv6Wp/CDFrhI6adHwfGY5ebL8GfCVe8HwlrR3CFpYuKtLw96ZnJZ."));
+
 	}
 
 }

4a2391b55f39060325d648aa55ca1e2692ac26c9
LIANG XIONG <xionglwh@cn.ibm.com>
2019-11-07 22:10:59 +0800

    update.

--- src/main/java/com/fsd/sba/mapper/UserMapper.java
+++ src/main/java/com/fsd/sba/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.fsd.sba.entity.User;
@@ -11,12 +12,12 @@ import com.fsd.sba.entity.User;
 @Mapper
 public interface UserMapper {
     @Select("SELECT * FROM USER WHERE EMAIL = #{email}")
-	User findByName(String email);
+	User findByName(@Param("email") String email);
     
     @Insert("INSERT INTO USER(EMAIL,FIRST_NAME,LAST_NAME, MOBILE, PASSWORD, ROLE, ACTIVE, AVATAR_PATH, BALANCE) "
     		+ "VALUES(#{email}, #{firstName}, #{lastName}, #{mobile}, #{password}, #{role}, #{active}, #{avatarPath}, #{balance})")
     void save(User user);
     
     @Select("SELECT * FROM USER WHERE ID IN (#{userIds}::int[])")
-    List<User> getUsersByIds(List<Long> userIds);
+    List<User> getUsersByIds(@Param("userIds") List<Long> userIds);
 }
--- src/test/java/com/fsd/sba/AccountApplicationTests.java
+++ src/test/java/com/fsd/sba/AccountApplicationTests.java
@@ -1,33 +1,21 @@
 package com.fsd.sba;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.fsd.sba.entity.User;
-
+//@RunWith(SpringRunner.class)
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
 public class AccountApplicationTests {
 
     
-    @Test
-    public void test() throws Exception {
-    	/*
-    	userRepository.findAll();
-    	userRepository.deleteAll();
-    	User user = new User("password", "admin@fsd.com", "fsd", "admin");
-
-    	userRepository.save(user);
-    	Assert.assertEquals(1, userRepository.findAll().size());
-    	Assert.assertEquals("admin@fsd.com", userRepository.findByName("admin").getEmail());
-    	*/
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 	
 }
